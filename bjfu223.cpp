7bfe8e2615ca499fef7f93c84264c44919481ae8
adolli <adolli@163.com>
2018-10-23 11:29:21 +0800

    format code
    Format code in K&R cpp style.

--- bjfu223.cpp
+++ bjfu223.cpp
@@ -7,46 +7,56 @@
 //
 
 #include <iostream>
-#include "cstdio"
-#include "cstring"
+#include <cstdio>
+#include <cstring>
 
 using namespace std;
 
-typedef struct Books{
+struct Books {
     char isbn[200];
     char name[200];
     float price;
-    struct Books *next = NULL;
-}Book;
+    Books *next = NULL;
+};
+
+typedef Books Book;
 
 //以第一个book作为头指针
 
-inline int getBook(Book &b){
-    scanf("%s%s%f",b.isbn, b.name, &b.price);
-    if (b.isbn[0]=='0'&&b.name[0]=='0'&&b.price<1e-6) {
+// actually, inline is not necessary in modern compilers
+inline int getBook(Book &b)
+{
+    scanf("%s%s%f", b.isbn, b.name, &b.price);
+    if (b.isbn[0] == '0' && b.name[0] == '0' && b.price < 1e-6) 
+    {
         return 0;
     }
     return 1;
 }
 
-inline void printBook(Book a){
+inline void printBook(Book a) 
+{
     printf("%s %s %.2f\n", a.isbn, a.name ,a.price);
 }
 
-void appendBook(Book &lis,Book &t){
+void appendBook(Book &lis, Book &t) 
+{
     Book* temp = &lis;
-    while (temp->next!=NULL) {
-        temp=temp->next;
+    while (temp->next != NULL) 
+    {
+        temp = temp->next;
     }
-    temp->next = new Book;
+    temp->next = new Book();
     strcpy(temp->next->isbn, t.isbn);
     strcpy(temp->next->name, t.name);
-    temp->next->price=t.price;
+    temp->next->price = t.price;
 }
 
-void insertBook(Book &lis, int n, Book &t){
+void insertBook(Book &lis, int n, Book &t)
+{
     Book *temp = lis.next;
-    for (int i=0; i<n-2; i++) {
+    for (int i = 0; i < n - 2; i++) 
+    {
         temp = temp->next;
     }
     Book *stemp;
@@ -55,9 +65,11 @@ void insertBook(Book &lis, int n, Book &t){
     t.next = stemp;
 }
 
-void deleteBook(Book &lis, int n){
+void deleteBook(Book &lis, int n)
+{
     Book *temp = lis.next;
-    for (int i=0; i<n-2; i++) {
+    for (int i = 0; i < n - 2; i++) 
+    {
         temp = temp->next;
     }
     Book *stemp;
@@ -66,12 +78,15 @@ void deleteBook(Book &lis, int n){
     temp->next = stemp;
 }
 
-int showBook(Book *lis){
-    if(lis->next==NULL){
+int showBook(Book *lis)
+{
+    if(lis->next == NULL)
+    {
         return 0;                    //return 0表示链表为空
     }
     Book *temp = lis->next;
-    while (temp!=NULL) {
+    while (temp != NULL) 
+    {
         printBook(*temp);
         temp = temp->next;
     }
@@ -79,7 +94,8 @@ int showBook(Book *lis){
 }
 
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
     Books lis;
     Book b;
     int count;
